99e30724b2f3d0ecb3cfb07d81e0f328fc7fea3b
h1mmeister <himanshu.yadav643@gmail.com>
2022-08-14 00:08:08 +0530

    working with application context

---
+++ demoProject/src/main/java/com/himanshu/main/Example1.java
@@ -0,0 +1,30 @@
+package com.himanshu.main;
+
+import com.himanshu.beans.Vehicle;
+import com.himanshu.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Honda City");
+        System.out.println("Vehicle name from non-spring context is: " + vehicle.getName());
+        /*
+        The var keyword was introduced in Java 10. Type inference is used in
+        var keyword in which it detects automatically the datatype of a variable
+        based on the surrounding context.
+        * */
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+
+        /*
+        We don’t need to do any explicit casting while fetching a bean from context.
+        Spring is smart enough to look for a bean of the type you requested in its context.
+        If such a bean doesn’t exist,Spring will throw an exception.
+        * */
+    }
+}
